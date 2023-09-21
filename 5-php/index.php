92d64dd99a0babc3aac86098366091ae9ce0ff5e
uc-ron <ronit.singh@ucertify.com>
2023-09-21 15:06:02 +0530

    booleans

---
+++ 5-php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// BOOLEANS
+
+$isComplete = true;
+
+// 0 -0 = false 
+// 0.0 -0.0 = false
+// '' = false
+// '0' = false
+// [] = false
+// null = false
+// almost  everything else 
+
+if ($isComplete) {
+    // do Something
+} else {
+    // do something else
+}
