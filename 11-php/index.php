bbedb37e221424398934cd45d18aeb2d2f05a39c
uc-ron <ronit.singh@ucertify.com>
2023-09-22 10:31:43 +0530

    operators part-2

--- 11-php/index.php
+++ 11-php/index.php
@@ -45,3 +45,33 @@ echo "<br>";
 echo "<br>";
 echo "************Bitwise Operator (& | ^ ~ << >>) ***************";
 echo "<br>";
+echo "#### It perform operation after binary conversion ####";
+echo "<br>";
+echo "#### here we use 6 and 3 ==>  110 and 011 ####";
+echo "<br>";
+
+$x = 6;
+$y = 3;
+var_dump($x & $y);
+var_dump($x | $y);
+var_dump($x ^ $y);
+var_dump(~$x & $y);
+var_dump($x >> 1);
+var_dump($x << 1);
+echo "<br>";
+
+
+echo "<br>";
+echo "************Array Operator (+ == === !== <> !=) ***************";
+echo "<br>";
+
+$x = ['a' => 1, 'b' => 2, 'c' => 3];
+$y = ['d' => 4, 'e' => 5, 'f' => 6];
+
+$z = $x + $y;
+print_r($z);
+echo "<br>";
+$x = ['a' => 1, 'b' => 2];
+$y = ['a' => '1', 'b' => 2];
+var_dump($x == $y);
+var_dump($x === $y);
